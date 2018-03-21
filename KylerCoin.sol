5ea4395f20ab6911982c4c7ac4b58659301ff76d
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2018-03-21 15:55:36 -0700

    Implement Bancor to KylerCoin.sol

--- KylerCoin.sol
+++ KylerCoin.sol
@@ -34,16 +34,24 @@ contract SafeMath {
     }
 }
 
-contract ERC223Token is ERC223, SafeMath, Balances, Utils {
+contract ERC223Token is ERC223, SafeMath, Balances, Utils, ISmartToken, Owned, TokenHolder {
 
     mapping(address => uint) balances;
 
-  string public standard = 'Token 0.1';
+  string public standard = '0.3';
   string public name = "KylerCoin";
   string public symbol = "KC";
   uint8 public decimals = 18;
   uint256 public totalSupply;
 
+  bool public transfersEnabled = true;    // true if transfer/transferFrom are enabled, false if not
+
+  // triggered when a smart token is deployed - the _token address is defined for forward compatibility, in case we want to trigger the event from a factory
+  event NewSmartToken(address _token);
+  // triggered when the total supply is increased
+  event Issuance(uint256 _amount);
+  // triggered when the total supply is decreased
+  event Destruction(uint256 _amount);
 
   // Function to access name of token .
   function name() public view returns (string _name) {
@@ -67,12 +75,12 @@ contract ERC223Token is ERC223, SafeMath, Balances, Utils {
    *
    * Initializes contract with initial supply tokens to the creator of the contract
    */
-   /**
+
   function ERC223Token(uint256 initialSupply) public {
       totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
       balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
+      NewSmartToken(address(this));
   }
-**/
 
   // Function that is called when a user or another contract wants to transfer funds .
   function transfer(address _to, uint _value, bytes _data, string _custom_fallback) public returns (bool success) {
@@ -104,7 +112,7 @@ contract ERC223Token is ERC223, SafeMath, Balances, Utils {
 
   // Standard function transfer similar to ERC20 transfer with no _data .
   // Added due to backwards compatibility reasons .
-  function transfer(address _to, uint _value) public returns (bool success) {
+  function transfer(address _to, uint _value) public transfersAllowed returns (bool success) {
 
     //standard function transfer similar to ERC20 transfer with no _data
     //added due to backwards compatibility reasons
@@ -151,4 +159,74 @@ contract ERC223Token is ERC223, SafeMath, Balances, Utils {
   function balanceOf(address _owner) public view returns (uint balance) {
     return balances[_owner];
   }
+
+  /**
+      @dev an account/contract attempts to get the coins
+      throws on any error rather then return a false flag to minimize user errors
+      in addition to the standard checks, the function throws if transfers are disabled
+
+      @param _from    source address
+      @param _to      target address
+      @param _value   transfer amount
+
+      @return true if the transfer was successful, false if it wasn't
+  */
+  function transferFrom(address _from, address _to, uint256 _value) public transfersAllowed returns (bool success) {
+      assert(super.transferFrom(_from, _to, _value));
+      return true;
+  }
+
+  // allows execution only when transfers aren't disabled
+  modifier transfersAllowed {
+      assert(transfersEnabled);
+      _;
+  }
+
+  /**
+      @dev disables/enables transfers
+      can only be called by the contract owner
+
+      @param _disable    true to disable transfers, false to enable them
+  */
+  function disableTransfers(bool _disable) public ownerOnly {
+      transfersEnabled = !_disable;
+  }
+
+  /**
+      @dev increases the token supply and sends the new tokens to an account
+      can only be called by the contract owner
+
+      @param _to         account to receive the new amount
+      @param _amount     amount to increase the supply by
+  */
+  function issue(address _to, uint256 _amount)
+      public
+      ownerOnly
+      validAddress(_to)
+      notThis(_to)
+  {
+      totalSupply = safeAdd(totalSupply, _amount);
+      balanceOf[_to] = safeAdd(balanceOf[_to], _amount);
+
+      Issuance(_amount);
+      Transfer(this, _to, _amount);
+  }
+
+  /**
+      @dev removes tokens from an account and decreases the token supply
+      can be called by the contract owner to destroy tokens from any account or by any holder to destroy tokens from his/her own account
+
+      @param _from       account to remove the amount from
+      @param _amount     amount to decrease the supply by
+  */
+  function destroy(address _from, uint256 _amount) public {
+      require(msg.sender == _from || msg.sender == owner); // validate input
+
+      balanceOf[_from] = safeSub(balanceOf[_from], _amount);
+      totalSupply = safeSub(totalSupply, _amount);
+
+      Transfer(_from, this, _amount);
+      Destruction(_amount);
+  }
+
 }
