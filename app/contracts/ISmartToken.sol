53bf27bd6cf524ad560bb84bab0907887b3c918c
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2018-03-22 22:24:22 -0700

    change ISmartToken.sol to ERC223 Interface

--- app/contracts/ISmartToken.sol
+++ app/contracts/ISmartToken.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.4.18;
 import './IOwned.sol';
-import './IERC20Token.sol';
+import './ERC223_Interface.sol';
 
 /*
     Smart Token interface
 */
-contract ISmartToken is IOwned, IERC20Token {
+contract ISmartToken is IOwned, ERC223 {
     function disableTransfers(bool _disable) public;
     function issue(address _to, uint256 _amount) public;
     function destroy(address _from, uint256 _amount) public;
