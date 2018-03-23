ecd260216c85202e1911e4d3326c6ccad139357c
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2018-03-22 22:28:49 -0700

    set Crowdsale rate to 0.01 ETH / Kylercoin

--- app/contracts/Crowdsale.sol
+++ app/contracts/Crowdsale.sol
@@ -45,12 +45,11 @@ contract Crowdsale {
    * @param _wallet Address where collected funds will be forwarded to
    * @param _token Address of the token being sold
    */
-  function Crowdsale(uint256 _rate, address _wallet, ERC20 _token) public {
-    require(_rate > 0);
+  function Crowdsale(address _wallet, ERC20 _token) public {
     require(_wallet != address(0));
     require(_token != address(0));
 
-    rate = _rate;
+    rate = 10000000000000000;
     wallet = _wallet;
     token = _token;
   }
